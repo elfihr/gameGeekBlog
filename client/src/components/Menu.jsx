61a4452bb471d748592efc50c637f641f1e9f9a4
Luiz <luizfelipe.vor@gmail.com>
2024-08-24 20:26:38 -0300

    button update
    navigate between posts

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,46 +1,52 @@
 import React, { useEffect, useState } from 'react'
 import FeedIcon from '@mui/icons-material/Feed';
 import axios from 'axios';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-const Menu = ({cat}) => {
-    const [posts, setPosts] = useState([]);
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const res = await axios.get(`/api/posts/?cat=${cat}`);
-          setPosts(res.data);
-        } catch (err) {
-          console.log(err);
-        }
-      };
-      fetchData();
-    }, [cat]);
-    return (
-        <section className='flex-2 flex-col gap-10'>
+const Menu = ({ cat }) => {
+  const [posts, setPosts] = useState([]);
 
-            <div className='flex flex-col items-center mb-[-1rem] gap-2'>
-                <div className='flex items-center'>
-                    <FeedIcon />
-                    <h1 className='text-h4 font-semibold'>Outras Postagens</h1>
-                </div>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/api/posts/?cat=${cat}`);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [cat]);
+  return (
+    <section className='flex-2 flex-col gap-10'>
 
-                <p>Que você pode Gostar <span className='text-red-500 text-p'>♥</span></p>
-                <div className='w-[80%] bord'></div>
-            </div>
+      <div className='flex flex-col items-center mb-[-1rem] gap-2'>
+        <div className='flex items-center'>
+          <FeedIcon />
+          <h1 className='text-h4 font-semibold'>Outras Postagens</h1>
+        </div>
 
-            {posts.map(post => {
-                return (
-                    <div key={post.id}>
-                        <img className='w-full h-40 object-cover' src={`../../public/upload/${post.img}`} alt={post.title} />
-                        <p className='font-semibold text-p'>{post.title}</p>
-                        <button className='btn'>Leia Mais</button>
-                    </div>
-                )
+        <p>Que você pode Gostar <span className='text-red-500 text-p'>♥</span></p>
+        <div className='w-[80%] bord'></div>
+      </div>
 
-            })}
-        </section>
-    )
+      {posts.map(post => {
+        return (
+          <div key={post.id}>
+            <img className='w-full h-40 object-cover' src={`../../public/upload/${post.img}`} alt={post.title} />
+            <p className='font-semibold text-p'>{post.title}</p>
+            <Button variant="contained">
+              <Link to={`/post/${post.id}`}>
+                Leia Mais
+              </Link>
+            </Button>
+          </div>
+        )
+
+      })}
+    </section>
+  )
 }
 
 export default Menu
